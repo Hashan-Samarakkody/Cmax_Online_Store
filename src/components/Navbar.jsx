39ba3ba032cef40ef8ebd1dc0db8e6bdadf58605
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-02-11 19:21:56 +0530

    Completed the "Add to cart" functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
 	const [visible, setVisible] = React.useState(false);
-	const {setShowSearch} = React.useContext(ShopContext);
+	const { setShowSearch, getCartCount } = React.useContext(ShopContext);
 
 	return (
 		<div className='relative flex items-center justify-between py-5 font-medium'>
@@ -47,7 +47,7 @@ const Navbar = () => {
 				</div>
 				<Link to='/cart' className='relative'>
 					<img src={assets.cart_icon} alt="" className='w-5 min-w-5 cursor-pointer' />
-					<p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[10.5px]'>10</p>
+					<p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[10.5px]'>{getCartCount()}</p>
 				</Link>
 				<img
 					onClick={() => setVisible(true)}
