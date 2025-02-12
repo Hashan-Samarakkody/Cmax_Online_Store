77fb332640803bb5fc247dea02643ee437a07817
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-02-12 10:09:44 +0530

    Finalize the "Cart" page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const { products, currency, cartItems, updateQuantity  } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate  } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -47,7 +48,7 @@ const Cart = () => {
             return (
               <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'> 
                 <div className='flex items-start gap-6'>
-                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                  <img className='w-16 sm:w-20 rounded-lg' src={productData.image[0]} alt="" />
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
                     <div className='flex items-center gap-5 mt-2'>
@@ -57,12 +58,21 @@ const Cart = () => {
                   </div>
                 </div>
 
-                <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
                 <img onClick={() => updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="" />
               </div>
             );
           })
         }
+      </div>
+
+      <div className='flex justify-end mt-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+          <div className='w-full text-end'>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3 rounded-sm'>PROCEED TO CHECKOUT</button>
+          </div>
+        </div>
       </div>
 
     </div>
