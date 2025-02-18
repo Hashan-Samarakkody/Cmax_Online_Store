fc7a1d39ea10aac46fc45dcf0152907cfb7e5d03
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-02-18 19:11:27 +0530

    Add product feature is partially completed

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,10 +1,13 @@
+import { v2 as cloudinary } from 'cloudinary';
+import productModel from '../models/productModel.js';
+
 // Add product function
 const addProduct = async (req, res) => {
 
     try {
 
         const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
-        
+
         const image1 = req.files.image1 && req.files.image1[0];
         const image2 = req.files.image2 && req.files.image2[0];
         const image3 = req.files.image3 && req.files.image3[0];
@@ -12,16 +15,35 @@ const addProduct = async (req, res) => {
 
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
 
-        let imagesUrl 
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let resullt = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
+                return resullt.secure_url;
+            }
+            ))
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller)
-        console.log(images)
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === 'true' ? true : false,
+            sizes: JSON.parse(sizes),
+            images: imagesUrl,
+            date: Date.now()
+        }
 
-        res.json({})
-        
+        console.log(productData)
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({success: true, message: 'Product added successfully!'});
+
     } catch (error) {
         console.log(error)
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
