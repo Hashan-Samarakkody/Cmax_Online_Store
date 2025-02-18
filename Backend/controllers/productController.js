55b9cd1d1f179cbc447cb18dfc68c6efe28401b3
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-02-18 19:25:59 +0530

    List products functionality was created

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -49,6 +49,17 @@ const addProduct = async (req, res) => {
 
 // Total product list function
 const listProduct = async (req, res) => {
+
+    try {
+
+        const products = await productModel.find({});
+        res.json({success: true, products});
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message }) 
+    }
+
 }
 
 // Remove product function
