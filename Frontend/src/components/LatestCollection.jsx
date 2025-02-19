be5878d77330fd70a8b0b7d98abb6f444b4c0e67
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-02-19 12:02:46 +0530

    Change file structure

---
+++ Frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+import ProductItem from './ProductItem'
+
+const LatestCollection = () => {
+
+  const { products } = useContext(ShopContext);
+  const [latetestProducts, setLatestCollection] = React.useState([]);
+
+  useEffect(() => {
+    setLatestCollection(products.slice(0,8));
+  }, []);
+
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTION'} />
+        <p className='w-3/4 m-auto text-sm sm:text-sm md:text-base text-gray-600'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam fuga molestiae natus,
+          maxime in recusandae reprehenderit! Similique, quos aspernatur, possimus quaerat earum
+          veritatis et asperiores inventore aliquid aperiam necessitatibus consequatur.
+        </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 gap-y-6'>
+        {
+          latetestProducts.map((item, index) => (
+            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
