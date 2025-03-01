9b7359fc8c83d537f794a8f2e250dde9cc081466
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2025-03-01 21:20:49 +0530

    Functionality for displaying all orders in admin panel is created

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { backendUrl, currency } from '../App'
+import { toast } from 'react-toastify'
+import { assets } from '../assets/assets'
 
-const Orders = () => {
+const Orders = ({ token }) => {
+
+  const [orders, setOrders] = useState([])
+
+  const fetchAllOrders = async () => {
+
+    if (!token) {
+      return null
+    }
+
+    try {
+      const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: { token } })
+
+      if (response.data.success) {
+        setOrders(response.data.orders)
+      } else {
+        toast.error(response.data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders()
+  }, [token])
+
   return (
-    <div>Orders</div>
+    <div>
+      <h3>Orders</h3>
+      <div>
+        {
+          orders.map((order, index) => (
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 rounded-sm' key={{ index }}>
+              <img className='w-12 rounded-xs' src={assets.parcel_icon} alt="" />
+              <div>
+                <div>
+                  {
+                    order.items.map((item, index) => {
+                      if (index === order.items.length - 1) {
+                        return <p className='py-0.5' key={index}>{item.name} <b>×</b> {item.quantity} <span>(Size: {item.size})</span></p>
+                      } else {
+                        return <p className='py-0.5' key={index}>{item.name} <b>×</b> {item.quantity} <span> (Size: {item.size})</span>,</p>
+                      }
+                    })
+                  }
+                </div>
+                <p className='mt-3 mb-2 font-medium'>{order.address.firstName + " " + order.address.lastName}</p>
+                <div>
+                  <p>{order.address.street + ","}</p>
+                  <p>{order.address.city + ", " + order.address.state + ", " + order.address.country + ", " + order.address.postalCode}</p>
+                </div>
+                <p>{order.address.phoneNumber}</p>
+              </div>
+              <div>
+                <p className='text-sm sm:text-[15px]'>Items: {order.items.length}</p>
+                <p className='mt-3'>Payment Method: {order.paymentMethod}</p>
+                <p>Payment: {order.payment ? "Done" : "Pending" }</p>
+                <p>Date: {new Date(order.date).toLocaleString()}</p>
+              </div>
+              <p className='text-sm sm:text-[15px]'>{currency}{order.amount}.00</p>
+              <select value={order.status} className='p-2 font-semibold'>
+                <option value="Order Placed">Order Placed</option>
+                <option value="Picking">Picking</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Out for Delivery">Out for Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
